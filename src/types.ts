e15d606da4b5447d57ace9254da9239480f0ed60
Nicholas Shankland <nicholas.m.shankland@gmail.com>
2022-02-18 13:20:38 -0800

    Added the edit distance algorithm in sub folder

---
+++ src/types.ts
@@ -0,0 +1,27 @@
+export enum Operation {
+  INSERT = "insert",
+  DELETE = "delete",
+  REPLACE = "replace",
+  INITIAL = "",
+}
+
+export type DpTable = Array<DpRow>;
+
+export type DpRow = Array<number>;
+export interface ErrorItem {
+  char: string;
+  index: number;
+  cost?: number;
+  operation: Operation;
+}
+export interface ErrorGroup {
+  errorString: string;
+  startIndex: number;
+  endIndex: number;
+  operation: Operation;
+}
+
+export interface CostItem {
+  cost: number;
+  operation: Operation;
+}
