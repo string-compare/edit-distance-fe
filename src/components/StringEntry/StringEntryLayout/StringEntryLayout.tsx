f3e82cc35c282c13bea78ea6e55ad741e0e99f3d
Justin Bird <justin.bird@dubsado.com>
2022-02-21 18:17:08 -0800

    Added layout component for StringEntry.

---
+++ src/components/StringEntry/StringEntryLayout/StringEntryLayout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./StringEntryLayout.module.css";
+
+const StringEntryLayout: React.FC = ({ children }) => (
+  <div className={styles.container} />
+);
+
+export default StringEntryLayout;
