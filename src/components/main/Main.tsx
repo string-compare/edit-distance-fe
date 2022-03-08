c3985e1cf97b003c4e910c9a4671210f026777b0
Nicholas Shankland <nicholas.m.shankland@gmail.com>
2022-03-07 18:38:27 -0800

    building to gh pages

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { StringEntry, StringResult } from "../";
 import styles from "./Main.module.css";
 import { useWebWorker } from "../../webworker/useWebworker";
