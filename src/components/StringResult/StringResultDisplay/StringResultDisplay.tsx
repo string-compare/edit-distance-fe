0b860194c9bb4193a741f2a98c9fe92aa3bdf981
Nicholas Shankland <nicholas.m.shankland@gmail.com>
2022-03-16 17:48:16 -0700

    deleted the string result file

--- src/components/StringResult/StringResultDisplay/StringResultDisplay.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./StringResultDisplay.module.css";
-
-const StringResultDisplay: React.FC = ({ children }) => (
-  <div className={styles.container}>{children}</div>
-);
-
-export default StringResultDisplay;
